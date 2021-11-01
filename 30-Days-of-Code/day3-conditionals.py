38b2711028dcbd2a1de0f2c4fa7a124f8de325be
Dana Rocha <46910672+dana-rocha@users.noreply.github.com>
2021-10-31 19:13:02 -0700

    Create day3-conditionals.py

---
+++ 30-Days-of-Code/day3-conditionals.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+import math
+import os
+import random
+import re
+import sys
+
+# Given an integer, n , perform the following conditional actions:
+# If n is odd, print Weird
+# If n is even and in the inclusive range of 2 to 5, print Not Weird
+# If n is even and in the inclusive range of 6 to 20, print Weird
+# If n is even and greater than 20, print Not Weird
+        
+        
+if __name__ == '__main__':
+    N = int(input().strip())
+    
+    if (N % 2 != 0):
+        print("Weird")
+    elif (N % 2 == 0) and (N in range(2, 6)):
+        print("Not Weird")
+    elif (N % 2 == 0) and (N in range(5, 21)):
+        print("Weird")
+    elif (N % 2 == 0) and (N > 20):
+        print("Not Weird")
