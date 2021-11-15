1df79d94c2de4089a9be615a84d64d70450e1197
Dana Rocha <46910672+dana-rocha@users.noreply.github.com>
2021-11-14 18:46:14 -0800

    Day 10 done

--- 30-Days-of-Code/day10-binary.py
+++ 30-Days-of-Code/day10-binary.py
@@ -11,25 +11,25 @@ import sys
 if __name__ == '__main__':
     n = int(input().strip())
 
-    
     consecutive = 0
-    max_num = 0
+    maximum = 0
+
     # Convert to binary
         # Divide the base-10 number by 2 and keep the remainder
         # Repeat until you reach 0
-    while (n > 0):
+    while n > 0:
         
         if n % 2 == 1:
             consecutive += 1
-            max_num += 1
             
-            if max_num > consecutive:
-                max_num = consecutive
             
+            if consecutive > maximum:
+                maximum = consecutive
+
         else:
             consecutive = 0
-            
+
         n = n // 2
-    
-    print(max_num)
-        
+
+    print(maximum)
+            
