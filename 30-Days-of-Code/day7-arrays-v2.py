fc2f6aee0d78363e68abc941ea47936743b8385e
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2022-05-28 15:54:57 -0700

    Reversing an array

---
+++ 30-Days-of-Code/day7-arrays-v2.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Task
+# Given an array, A , of N integers, print A's elements 
+# in reverse order as a single line of space-separated numbers.
+
+# Example:
+# A = [1, 2, 3, 4]
+# Print: 4 3 2 1 where each integer is separated by one space
+
+# Constraints:
+# 1 <= N <= 1000
+# 1 <= A[i] <= 10000 where A[i] is the ith integer in the array
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+    
+    
+    result = ""
+    for i in range(len(arr) -1 , -1 , -1):
+        result += str(arr[i]) + " "
+
+    print(result.rstrip())
